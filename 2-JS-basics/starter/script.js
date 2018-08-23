ff6d1ffe4f8c8c06ae8c385a4c7b1900695f7a11
Jose Perales <joseperales@Joses-MacBook-Pro.local>
2018-08-23 15:05:37 -0500

    Completed the second quiz

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -31,3 +31,31 @@ var johnsBMI = johnsMass/(johnsHeight * johnsHeight);
 console.log(marksBMI>johnsBMI);
 console.log("Mark's BMI is: " + marksBMI);
 console.log("John's BMI is: "+ johnsBMI);
+
+//Boolean logic
+yearJose >= 21 ? console.log("Jose is legal")
+: console.log(" Can not drink");
+
+
+var johnGame1 = 89;
+var johnGame2 = 120;
+var johnGame3 = 103;
+var johnsAvg; 
+
+var marksGame1 = 116;
+var marksGame2 = 94;
+var marksGame3 = 123;
+var marksAvg;
+
+//Calculating John's average 
+johnsAvg = (johnGame1+johnGame2+johnGame3) /3;
+//Calculating Marks's average 
+marksAvg = (marksGame1 + marksGame2 + marksGame3) / 3; 
+
+//Printing winner
+console.log("John's Score: "+johnsAvg);
+console.log("Mark's Sore: "+marksAvg);
+
+johnsAvg > marksAvg ? console.log("John wins in average points: "+johnsAvg)
+: console.log("Mark wins in average points: " + marksAvg);
+
